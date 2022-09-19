c8eec05ec5f8a8e2e8e85fc2e347bd255652aff3
Pablo Quijano <paquijar@MacBook-Pro-de-Pablo.local>
2022-09-18 23:29:31 -0500

    Foto tamano

--- components/Item.tsx
+++ components/Item.tsx
@@ -14,7 +14,7 @@ const Item = ({ title, image, link }: ItemProps) => {
       <a>
         <div className="bg-red-400 h-48 rounded-lg flex justify-between items-center pl-4">
           <h1 className="text-center text-3xl capitalize font-bold">{title}</h1>
-          <div className="w-1/4 h-full relative rounded-r-lg">
+          <div className="w-1/3 h-full relative rounded-r-lg">
             <Image
               src={image}
               alt={title}
